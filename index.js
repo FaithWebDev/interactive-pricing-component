96f32568bc0f4c2b51548a262cc89550695308c0
imFaith <44627279+imFaith@users.noreply.github.com>
2022-09-13 23:48:58 -0400

    added a few styling changes & set price # to fixed(2)

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 //grab slider , per-month , toggle and view/$$ from html
 const slider = document.querySelector('.slider');
-let defaultValue = slider.defaultValue;
-let currentValue = slider.value;
 const toggleBtn = document.querySelector('#toggle-btn');
-//set default toggle to false
+//set default discount toggle to false
 toggleBtn.checked = false;
 const pageViews = document.querySelector('#page-views');
 const price = document.querySelector('#price');
 //sets the default value on loading the page to 16 
-price.innerText = '$16';
+price.innerText = '$16.00';
 //set default pageviews text
 pageViews.innerText = '100K PAGEVIEWS';
 
 
+//event listeners for each element
+slider.addEventListener('input', setCurrentValues)
+slider.addEventListener('input', colorSlider)
+toggleBtn.addEventListener('change', setCurrentValues)
+
+
 //make the price/views values update with slider movement
 function setCurrentValues() {
-    current = slider.value;
+    current = parseFloat(slider.value).toFixed(2)
     price.innerText = `$${current}`;
-    const views = Math.round((current * 6250) / 1000);
+    const views = Math.round((current * 6250) / 1000)
     if (slider.value < 100, 000) {
         pageViews.innerText = `${views} PAGEVIEWS`;
     } else {
         pageViews.innerText = `${views}K PAGEVIEWS`;
     }
-}
-
-
-//apply discount if toggle is checked
-function applyDiscount() {
-    const current = slider.value;
-    const discount = current - (current * .25);
-    if (toggleBtn.checked === true) {
-        price.innerText = `$${discount}`;
-    } else {
-        price.innerText = `$${current}`;
+    function applyDiscount() {
+        // const current = slider.value;
+        const discount = (current - (current * .25)).toFixed(2);
+        if (toggleBtn.checked === true) {
+            price.innerText = `$${discount}`;
+        } else {
+            price.innerText = `$${current}`;
+        }
     }
+    applyDiscount()
 }
 
-// linear-gradient(-90deg, hsl(224, 65%, 95%) 50%, hsl(174, 86%, 45%) 30%)
+
 // make the slider bar fill with color
 function colorSlider() {
     valPercent = (slider.value / slider.max) * 100;
